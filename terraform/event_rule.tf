2c51579397f46d846b29d266da41e395a8f0b0ff
Mi3-14159 <Mi3.1415926@gmail.com>
2016-09-01 09:36:04 +0200

    teraform fmt

--- terraform/event_rule.tf
+++ terraform/event_rule.tf
@@ -8,17 +8,14 @@ resource "aws_cloudwatch_event_target" "scheduled_ecr_cleaner_lambda" {
   rule  = "${aws_cloudwatch_event_rule.scheduled_ecr_cleaner.name}"
   arn   = "${aws_lambda_function.lambda.arn}"
   input = "${data.template_file.event_json.rendered}"
-
-  
 }
-
 
 data "template_file" "event_json" {
   template = "${file("${path.module}/event_json.json")}"
 
   vars = {
     aws_region = "${var.repo_region}"
-    dry_run = "${var.dry_run}"
+    dry_run    = "${var.dry_run}"
     repository = "${var.repository}"
   }
 }
